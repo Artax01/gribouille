17668c4816c23081a11087076bf7ec54df63b0c0
Artax01 <158834512+Artax01@users.noreply.github.com>
2025-04-01 17:53:53 +0200

    Ajout du GridPane et du contrôleur GrilleController avec initialisation de la grille

---
+++ tp3/src/main/java/iut/gon/tp3/GrilleController.java
@@ -0,0 +1,19 @@
+package iut.gon.tp3;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.GridPane;
+
+public class GrilleController implements Initializable {
+	@FXML
+	private GridPane grille;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		grille.setStyle("-fx-background-color: seashell");
+	}
+	
+}
