9c793fc5a3d29a9cbbbe2185df02b84144b7dacd
Artax01 <158834512+Artax01@users.noreply.github.com>
2025-04-01 16:17:48 +0200

    merge gribouille_tp2 -> gribouille_stable

---
+++ gribouille/src/main/java/iut/gon/gribouille/Dialogues.java
@@ -0,0 +1,19 @@
+package iut.gon.gribouille;
+
+import java.util.Optional;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+
+public class Dialogues {
+	public static boolean confirmation() {
+		Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Voulez-vous vraiment fermer l'application ?", ButtonType.YES, ButtonType.NO);
+		alert.setTitle("Fermeture de l'application");
+		Optional<ButtonType> result = alert.showAndWait();
+		
+		if (result.get() == ButtonType.NO) {
+			return false;
+		}
+		return true;
+	}
+}
