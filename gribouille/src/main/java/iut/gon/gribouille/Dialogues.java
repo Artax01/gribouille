25945b98a879792fd73ba556e569d5cf37fcd8e3
Artax01 <158834512+Artax01@users.noreply.github.com>
2025-04-01 15:52:58 +0200

    fix ButtonType.NO on Alert in Dialogues class

--- gribouille/src/main/java/iut/gon/gribouille/Dialogues.java
+++ gribouille/src/main/java/iut/gon/gribouille/Dialogues.java
@@ -7,7 +7,7 @@ import javafx.scene.control.ButtonType;
 
 public class Dialogues {
 	public static boolean confirmation() {
-		Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Voulez-vous vraiment fermer l'application ?", ButtonType.YES, ButtonType.YES);
+		Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Voulez-vous vraiment fermer l'application ?", ButtonType.YES, ButtonType.NO);
 		alert.setTitle("Fermeture de l'application");
 		Optional<ButtonType> result = alert.showAndWait();
 		
