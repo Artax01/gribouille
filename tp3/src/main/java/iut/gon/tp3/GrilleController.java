11edc9d9d68efbb48ea74251029a832057c0792c
Artax01 <158834512+Artax01@users.noreply.github.com>
2025-04-23 17:08:36 +0200

    Ajout de labels 3x3, modification de la taille et de l'alignement des labels dans chaque cellules du GridPane

--- tp3/src/main/java/iut/gon/tp3/GrilleController.java
+++ tp3/src/main/java/iut/gon/tp3/GrilleController.java
@@ -5,6 +5,8 @@ import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 
 public class GrilleController implements Initializable {
@@ -14,6 +16,22 @@ public class GrilleController implements Initializable {
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		grille.setStyle("-fx-background-color: seashell");
+		for (int lg = 0; lg < 3; lg++) {
+			for (int col = 0; col < 3; col++) {
+				Label label = new Label(String.format("L%dC%d", lg, col));
+				
+				label.setMaxWidth(1000);
+				label.setMaxHeight(1000);
+				label.setAlignment(Pos.CENTER);
+				
+				label.setOnMouseClicked(event -> {
+					label.setText("bonjour");
+				});
+				
+				grille.add(label, col, lg);
+			}
+		}
+
 	}
 	
 }
