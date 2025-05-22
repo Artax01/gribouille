790d0191fdee34346d3cb1f3bac29f4e6a26b29c
Artax01 <158834512+Artax01@users.noreply.github.com>
2025-05-22 12:34:30 +0200

    Ajout des cellules editables pour la quantité et le produit

--- tp6/src/main/java/fr/iutgon/tp6/FactureController.java
+++ tp6/src/main/java/fr/iutgon/tp6/FactureController.java
@@ -1,25 +1,19 @@
 package fr.iutgon.tp6;
 
 import fr.iutgon.tp6.modele.FabriqueProduits;
+
 import fr.iutgon.tp6.modele.Ligne;
 import fr.iutgon.tp6.modele.Produit;
 import javafx.beans.binding.Bindings;
-import javafx.beans.binding.NumberExpression;
-import javafx.beans.property.ReadOnlyIntegerWrapper;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.css.PseudoClass;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
-import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.ChoiceBoxTableCell;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
-import javafx.util.Callback;
 import javafx.util.StringConverter;
 import javafx.util.converter.IntegerStringConverter;
 
@@ -46,26 +40,39 @@ public class FactureController implements Initializable {
   @Override
   public void initialize(URL location, ResourceBundle resources) {
 	  qte.setCellValueFactory(new PropertyValueFactory<>("qte"));
-	    
-	    produit.setCellValueFactory(param -> {
-			return param.getValue().produitProperty();
-			// bindings.select dans le futur au lieu de la ligne au dessus
-	    });
-	    
-	    prixUnitaire.setCellValueFactory(param -> {
-			return param.getValue().getProduit().prixProperty();
-			// bindings.select dans le futur au lieu de la ligne au dessus
-	    });
-	    
-	    totalHT.setCellValueFactory(param -> {
-	    	return param.getValue().totalHTProperty();
-	    	// bindings.select dans le futur au lieu de la ligne au dessus
-	    });
-	    
-	    totalTTC.setCellValueFactory(param -> {
-	    	return param.getValue().totalTTCProperty();
-	    	// bindings.select dans le futur au lieu de la ligne au dessus
-	    });
+	  qte.setCellFactory(cell -> new TextFieldTableCell<>(new IntegerStringConverter()));
+	  
+	  produit.setCellValueFactory(param -> {
+		  return param.getValue().produitProperty(); // bindings.select dans le futur au lieu de la ligne au dessus
+	  });
+	  produit.setCellFactory(cell -> new ChoiceBoxTableCell<>(new StringConverter<Produit>() {
+			  	@Override
+				public String toString(Produit produit) {
+					return produit == null ? "" : produit.toString();
+				}
+	
+				@Override
+				public Produit fromString(String string) {
+					return FabriqueProduits.getProduits()
+						.stream()
+						.filter(p -> p.toString().equals(string))
+						.findFirst()
+						.orElse(null);
+				}
+	  		}, FXCollections.observableArrayList(FabriqueProduits.getProduits())
+	  ));
+	
+	  prixUnitaire.setCellValueFactory(param -> {
+		  return Bindings.select(param.getValue().produitProperty(), "prix");
+	  });
+	
+	  totalHT.setCellValueFactory(param -> { 
+		  return param.getValue().totalHTProperty();
+	  });
+	
+	  totalTTC.setCellValueFactory(param -> {
+		  return param.getValue().totalTTCProperty();
+	  });
   }
 
   public void onAjouter(ActionEvent actionEvent) {
