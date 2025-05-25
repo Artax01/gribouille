c883dc64eb108ab6d86dad5174e2efb13364cbfc
Artax01 <158834512+Artax01@users.noreply.github.com>
2025-05-25 23:50:37 +0200

    Ajout de la gestion de fin de partie avec une Alert pour relancer ou non une partie

--- tp5/src/main/java/fr/unicaen/iut/tp5/ControleurDemineur.java
+++ tp5/src/main/java/fr/unicaen/iut/tp5/ControleurDemineur.java
@@ -1,11 +1,14 @@
 package fr.unicaen.iut.tp5;
 
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
@@ -30,8 +33,6 @@ public class ControleurDemineur implements Initializable {
 		diff.selectedToggleProperty().addListener((observableValue, oldValue, newValue) -> {
 			if (newValue != null) {
 				initGrille(newValue.getUserData().toString());
-				nbInconnues.textProperty().bind(modele.nbInconnuesProperty().asString());
-				nbMarques.textProperty().bind(modele.nbMarquesProperty().asString());
 			}
 		});
 	}
@@ -43,10 +44,13 @@ public class ControleurDemineur implements Initializable {
 		
 		int[] donnees = ModeleDemineur.parseUserData(difficulty);
 		modele = new ModeleDemineur(donnees[0], donnees[1], donnees[2]);
+		nbInconnues.textProperty().bind(modele.nbInconnuesProperty().asString());
+		nbMarques.textProperty().bind(modele.nbMarquesProperty().asString());
 		
 		for (int i = 0; i < donnees[0]; i++) {
 			grid.getRowConstraints().add(new RowConstraints(32));
 		}
+		
 		for (int j = 0; j < donnees[1]; j++) {
 			grid.getColumnConstraints().add(new ColumnConstraints(32));
 		}
@@ -61,6 +65,7 @@ public class ControleurDemineur implements Initializable {
 				
 				int x = i;
 				int y = j;
+				
 				label.addEventHandler(MouseEvent.MOUSE_CLICKED, (evt) -> {
 					if (evt.getButton() == MouseButton.PRIMARY) {
 						modele.revele(x, y);
@@ -69,6 +74,20 @@ public class ControleurDemineur implements Initializable {
 						modele.marque(x, y);
 					}
 					label.setBackground(BackgroundType.getBackgroundOnText(modele.getText(x, y)));
+					
+					if (modele.getText(x, y).equals("X")) {
+						Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Voulez-vous relancer une partie avec la même difficuté ?", ButtonType.YES, ButtonType.NO);
+						alert.setTitle("Rejouer ?");
+						alert.setHeaderText("Vous avez perdu !");
+						Optional<ButtonType> result = alert.showAndWait();
+						
+						if (result.get() == ButtonType.NO) {
+							alert.close();
+						} else {
+							initGrille(difficulty);
+						}
+					}
+
 				});
 				
 				grid.add(label, j, i);
